0a3079064d869d6255f6afb789055b8c1b859175
Chris Laughlin <bluntm@gmail.com>
2020-04-15 19:54:24 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# whatdayisit
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./styles.css";
+import useFetch from "@chrislaughlin/usefetch";
+
+import styled from "styled-components";
+
+import WhatGif from "./whatday.gif";
+
+const StyledTitle = styled.h1`
+  font-size: 70px;
+  color: #ba2640;
+  font-family: "Permanent Marker", cursive;
+  text-align: center;
+`;
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledTheDay = styled.span`
+  font-family: "Permanent Marker", cursive;
+  font-size: 30px;
+  color: #fa2a00;
+`;
+
+const App = () => {
+  const theDay = new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(
+    new Date()
+  );
+
+  const { data } = useFetch("https://extreme-ip-lookup.com/json/");
+
+  return (
+    <StyledContainer>
+      <StyledTitle>What fucking day is it?</StyledTitle>
+      <div>
+        <img
+          src={WhatGif}
+          alt="monday no tuesday no wednesday what day? Thursday"
+        />
+      </div>
+      <p>
+        <StyledTheDay>{theDay}</StyledTheDay>
+      </p>
+      <p>
+        <StyledTheDay>{data && data.city}</StyledTheDay>
+      </p>
+    </StyledContainer>
+  );
+};
+
+export default App;
